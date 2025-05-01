3d4c52ef19849114c5ea70e0ccd14a7f40bbc8a8
shan1iu <mountainliuliu@gmail.com>
2025-05-01 23:53:38 +0800

    feat(tailwindcss): integrate Tailwind CSS and Daisy UI into Nuxt project
    - Add Tailwind CSS and Daisy UI configuration to Nuxt project
    - Create main.css file with Tailwind and Daisy UI import
    - Update App.vue to include Tailwind styles and Icon component
    - Add @nuxt/icon module and related dependencies
    - Configure Tailwind CSS in nuxt.config.ts

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      Primary
+    </button>
+    <Icon name="tabler:map" />
   </div>
 </template>
