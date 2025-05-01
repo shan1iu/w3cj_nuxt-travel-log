077ee1cd7fa2da59e3b19ba734c4ea79374ae888
shan1iu <mountainliuliu@gmail.com>
2025-05-02 00:05:29 +0800

    refactor: set up basic app structure and layout
    - Remove placeholder content from app.vue
    - Create nav-bar component for navigation
    - Set up default layout with header and main content area
    - Add home page with Travel Log app introduction

--- app.vue
+++ app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      Primary
-    </button>
-    <Icon name="tabler:map" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
