3bac468353a7246988d6df58152b423c7b185949
shan1iu <mountainliuliu@gmail.com>
2025-05-30 23:48:16 +0800

    feat(location): add unique slug generation for locations
    - Implement unique name constraint per user
    - Add slug generation using 'slugify' and 'nanoid'
    - Update location creation logic to check for existing slugs
    - Return 409 error if location name already exists

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,9 @@
 import type { DrizzleError } from "drizzle-orm";
 
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "~/lib/db";
 import { InsertLocation, location } from "~/lib/db/schema";
 
@@ -32,10 +36,39 @@ export default defineEventHandler(async (event) => {
       data,
     }));
   }
+
+  const existingLocation = !!(await db.query.location.findFirst({
+    where: and(
+      eq(location.name, result.data.name),
+      eq(location.userId, event.context.user.id),
+    ),
+  }));
+
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "A location with that name already exists.",
+    }));
+  }
+
+  let slug = slugify(result.data.name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+  while (existing) {
+    const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+    const idSlug = `${slug}-${nanoid}}`;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
   try {
     const [created] = await db.insert(location).values({
       ...result.data,
-      slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
+      slug,
       userId: event.context.user.id,
     }).returning();
     return created;
